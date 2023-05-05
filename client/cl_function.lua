14a5dfeb76391577acc99efc2a5823405df1b76e
wobozkyng <42249669+wobozkyng@users.noreply.github.com>
2023-05-05 19:48:31 +0700

    added cl_function.lua file

---
+++ client/cl_function.lua
@@ -0,0 +1,10 @@
+function labelText(text, ...)
+    local library = Config.translation[Config.Locale]
+    if library == nil then
+        return ("Translation [%s] does not exist"):format(Config.Locale)
+    end
+    if library[text] == nil then
+        return ("Translation [%s][%s] does not exist"):format(Config.Locale, text)
+    end
+    return library[text]:format(...) 
+end
--- config.lua
+++ config.lua
@@ -29,14 +29,3 @@ Config.translation = {
         ["wiped_vehicle"] = "%s vehicles have been wiped"
     }
 }
-
-function labelText(text, ...)
-    local library = Config.translation[Config.Locale]
-    if library == nil then
-        return ("Translation [%s] does not exist"):format(Config.Locale)
-    end
-    if library[text] == nil then
-        return ("Translation [%s][%s] does not exist"):format(Config.Locale, text)
-    end
-    return library[text]:format(...) 
-end
